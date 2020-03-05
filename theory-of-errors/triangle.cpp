f8612adacb09167a56dc7dca1324a79c5a0ba47f
Kamil Marut <kakkmar@gmail.com>
2020-03-05 17:44:35 +0100

    Fix area accuracy formula

--- theory-of-errors/triangle.cpp
+++ theory-of-errors/triangle.cpp
@@ -13,8 +13,20 @@ float triangle_area(float a, float b, float c) {
     );
 }
 
-float area_df(float a, float b, float c) {
+float area_df_a(float a, float b, float c) {
     return (-pow(a, 3) + 3 * pow(a, 2) * (b + c) - a * (pow(b, 2) + 4 * b * c + pow(c, 2)) - pow(b + c, 3)) 
+           / 
+           (2 * sqrt(-(a - b - 3 * c) * (a - 3 * b - c) * (a - b - c) * (a + b + c)));
+}
+
+float area_df_b(float a, float b, float c) {
+    return (pow(a, 3) - pow(a, 2) * (b + 2 * c) - 3 * a * pow(b + c, 2) + (b + c) * (3 * pow(b, 2) + 9 * b * c + 4 * pow(c, 2))) 
+           / 
+           (2 * sqrt(-(a - b - 3 * c) * (a - 3 * b - c) * (a - b - c) * (a + b + c)));
+}
+
+float area_df_c(float a, float b, float c) {
+    return (pow(a, 3) - pow(a, 2) * (2 * b + c) - 3 * a * pow(b + c, 2) + (b + c) * (4 * pow(b, 2) + 9 * b * c + 3 * pow(c, 2))) 
            / 
            (2 * sqrt(-(a - b - 3 * c) * (a - 3 * b - c) * (a - b - c) * (a + b + c)));
 }
@@ -39,10 +51,9 @@ int main() {
     cout << "[3] Please enter the accuracy of the given length." << endl;
     cin >> deltaC;
 
-    float area, areaDf, areaAccuracy;
+    float area, areaAccuracy;
     area = triangle_area(a, b, c);
-    areaDf = fabs(area_df(a, b, c));
-    areaAccuracy = areaDf * deltaA + areaDf * deltaB + areaDf * deltaC;
+    areaAccuracy = fabs(area_df_a(a, b, c) * deltaA + area_df_b(a, b, c) * deltaB + area_df_c(a, b, c) * deltaC);
 
     cout << endl << "A = " << area << " +- " << areaAccuracy << endl;
 
